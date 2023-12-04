1c47340ffb010faa34aaea082e1f0c8f1a65918b
graverealm <109730545+muzscripter@users.noreply.github.com>
2023-12-04 09:23:48 -0500

    Create data_template.lua

---
+++ data_template.lua
@@ -0,0 +1,21 @@
+local ClientData = {
+    Name = '';
+    Race = 'Human';
+    Inventory = {};
+    Rank = 'Human';
+
+    Combat = {
+        Moveset = nil;
+        UnlockedSkills = {
+            ['X'] = nil;
+            ['Y'] = nil;
+            ['Z'] = nil;
+        }
+    };
+
+    Stats = {
+        Breathing = {};
+        Speed = {};
+        Combat = {};
+    };
+}
