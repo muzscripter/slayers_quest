691e92b19f595d9e5049764ecf7b9817f0d65b58
graverealm <109730545+muzscripter@users.noreply.github.com>
2023-12-07 11:29:42 -0500

    Update projectile_module.lua

--- projectile_module.lua
+++ projectile_module.lua
@@ -1,1 +1,19 @@
+local Module = {};
 
+export type Projectile: any = {
+    Instance: Instance,
+    Speed: number,
+    Vector: Vector3
+}
+
+function Module:LinearProjectile()
+end
+
+function Module:CurvedProjectile()
+end
+
+function Module:AngledProjectile()
+end
+
+function 
+return Module
